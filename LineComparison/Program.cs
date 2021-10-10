1ec86beddfbfa3b3e93cb832b3d29a93a3db22ae
SHREESH PANDEY <shvspandey@gmail.com>
2021-10-10 18:38:59 +0530

    Length of a Line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,7 +6,18 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Please Enter the Values of X1,X2,Y1,Y2");
+            double x1 = Console.Read();
+            double x2 = Console.Read();
+            double y1 = Console.Read();
+            double y2 = Console.Read();
+            double LengthofX = (x2 - x1);
+            double LengthofY = (y2 = y1);
+            double PowX = Math.Pow(LengthofX, 2);
+            double PowY = Math.Pow(LengthofY, 2);
+            double LengthSq = PowX + PowY;
+            double LengthofLine = Math.Sqrt(LengthSq);
+            Console.WriteLine("Length of Line=" + LengthofLine);
         }
     }
 }
